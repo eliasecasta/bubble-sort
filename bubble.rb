3d5e3a1be67a56f143c17a2d130cb8b1640d707b
Elias Castaneda <e71az@icloud.com>
2020-06-19 09:46:56 -0600

    Improved the loop
    Switched from the unless loop and until to times loop syntax, changing the form of overwriting the array.

--- bubble.rb
+++ bubble.rb
@@ -1,23 +1,14 @@
 array=[4,3,2,7,8,10,2,1,0,5,20,28,69]
 
-def bubble_sort (array)
-  i= 0
-  unless i == array.length-1
-    j = i + 1
-    until j == array.length
-      first_value = array[i]
-      second_value = array[j]
-      if first_value > second_value
-        array[i] = second_value
-        array[j] = first_value
-        i -= 1
-        break
-      end
-      j += 1
+def bubble_sort(array)
+    (array.length-1).times do |i|
+        (array.length-1-i).times do |index|
+            if array[index] > array[index+1]
+                array[index], array[index+1]=array[index+1],array[index]
+            end
+        end
     end
-    i += 1
-  end
-  array
+    array
 end
 
 p bubble_sort(array)
