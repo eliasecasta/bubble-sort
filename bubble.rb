74173f6d797adcf76a688f2736e3bd7b1945635e
jessicafarias <jessica.farias.rosado@gmail.com>
2020-06-19 10:06:21 -0500

    Improve code readability

--- bubble.rb
+++ bubble.rb
@@ -1,16 +1,16 @@
-array=[4,3,2,7,8,0,0,5,7,8]
+array=[4,3,2,7,8,10,2,1,0,5,20,28,69]
 
 def bubble_sort (array)
   i= 0
-  while i != array.length-1
-    j= i + 1
-    while j != array.length
+  unless i == array.length-1
+    j = i + 1
+    until j == array.length
       first_value = array[i]
       second_value = array[j]
       if first_value > second_value
         array[i] = second_value
         array[j] = first_value
-        i-= 1
+        i -= 1
         break
       end
       j += 1
