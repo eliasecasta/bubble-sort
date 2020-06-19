25c7bcc6292a3cb98c26becc1940c6e0c6f73569
jessicafarias <jessica.farias.rosado@gmail.com>
2020-06-19 12:58:46 -0500

    Created sort_by function using yield

--- bubble.rb
+++ bubble.rb
@@ -1,4 +1,5 @@
-array=[4,3,2,7,8,10,2,1,0,5,20,28,69]
+array = %w[h44 he fgh jshkdal s sds]
+array2=[4,3,2,7,8,10,2,1,0,5,20,28,69]
 
 def bubble_sort(array)
     (array.length-1).times do |i|
@@ -11,4 +12,20 @@ def bubble_sort(array)
     array
 end
 
-p bubble_sort(array)
+def bubblue_sort_by(array)
+  (array.length - 1).times do |i|
+    (array.length - 1 - i).times do |index|
+      if (yield array[index], array[index + 1]) >= 1
+        array[index], array[index + 1] = array[index + 1], array[index]
+      end
+    end
+  end
+  array
+end
+
+x = bubblue_sort_by(array) do |left, right|
+  left.length - right.length
+end
+
+p bubble_sort(array2)
+p x
