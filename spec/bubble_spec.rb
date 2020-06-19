40a2986d0fabca99b79fed3b0cffd332ae81b2d6
Elias Castaneda <e71az@icloud.com>
2020-06-19 14:14:51 -0600

    Rubocop testing

--- spec/bubble_spec.rb
+++ spec/bubble_spec.rb
@@ -1,7 +1,7 @@
 array = %w[hi hello hey]
 array2 = [0, 2, 2, 3, 4, 78]
 
-def bubble_sort(array)
+def bubble_sort(array2)
   (array.length - 1).times do |i|
     (array.length - 1 - i).times do |index|
       array[index], array[index + 1] = array[index + 1], array[index] if array[index] > array[index + 1]
@@ -10,9 +10,9 @@ def bubble_sort(array)
   array
 end
 
-def bubble_sort_by(array)
-  (array.length-1).times do |i|
-    (array.length-1 - i).times do |index|
+def bubble_sort_by(array2)
+  (array.length - 1 ).times do |i|
+    (array.length - 1 - i).times do |index|
       array[index], array[index + 1] = array[index + 1], array[index] if (yield array[index], array[index + 1]) >= 1
     end
   end
