03342f9f3dda0ad9bbd7973a4d19d863b10cf7e8
Elias Castaneda <e71az@icloud.com>
2020-06-19 14:21:02 -0600

    rspec test

--- spec/bubble_spec.rb
+++ spec/bubble_spec.rb
@@ -1,16 +1,16 @@
 array = %w[hi hello hey]
 array2 = [0, 2, 2, 3, 4, 78]
 
-def bubble_sort(array2)
+def bubble_sort(array)
   (array.length - 1).times do |i|
     (array.length - 1 - i).times do |index|
-      array[index], array[index + 1] = array[index + 1], array[index] if array[index] > array[index + 1]
+      array[index], array[i + 1] = array[index + 1], array[index] if array[index] > array[index + 1]
     end
   end
   array
 end
 
-def bubble_sort_by(array2)
+def bubble_sort_by(array)
   (array.length - 1).times do |i|
     (array.length - 1 - i).times do |index|
       array[index], array[index + 1] = array[index + 1], array[index] if (yield array[index], array[index + 1]) >= 1
